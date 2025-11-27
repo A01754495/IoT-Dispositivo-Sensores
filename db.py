ca5d123f8e559e4baa3445a7a2c01089ad2cbad2
a01754495 <a01754495@tec.mx>
2025-11-27 10:38:26 -0600

    Añadido función get_measured_data(fromDate, toDate)

--- db.py
+++ db.py
@@ -1,13 +1,14 @@
 import mysql.connector
-
+from datetime import date, time
+from pandas import DataFrame, Series
 
 # --- CONEXION SQL ---
 def get_connection():
     return mysql.connector.connect(
         host="localhost", 
-        user="root",
-        password="",
-        database="reto2"
+        user="esp32",
+        password="ESP32-IoT-equipo1",
+        database="Reto"
     )
 
 
@@ -18,9 +19,9 @@ def get_latest_data():
 
     cursor.execute("""
         SELECT 
-            (SELECT medida_temp FROM registro_temp ORDER BY fecha DESC, hora DESC LIMIT 1) AS temp,
-            (SELECT medida_humedad FROM registro_humedad ORDER BY fecha DESC, hora DESC LIMIT 1) AS humedad,
-            (SELECT medida_gas FROM registro_gas ORDER BY fecha DESC, hora DESC LIMIT 1) AS gas
+            (SELECT medida_temp FROM Registro_temp ORDER BY fecha DESC, hora DESC LIMIT 1) AS temp,
+            (SELECT medida_humedad FROM Registro_humedad ORDER BY fecha DESC, hora DESC LIMIT 1) AS humedad,
+            (SELECT medida_gas FROM Registro_gas ORDER BY fecha DESC, hora DESC LIMIT 1) AS gas
     """)
 
     data = cursor.fetchone()
@@ -54,3 +55,38 @@ def get_sensor_locations():
             print(f"ERROR en lugar: {r['lugar']}")
 
     return sensors
+
+# --- OBTENER VALORES MEDIDOS ---
+def get_measured_data(fromDate, toDate): # el formato de fecha debe estar así: "YYYY-MM-DD"
+    conn = get_connection()
+    cursor = conn.cursor(dictionary=True)
+
+    fromDate = fromDate.split("-")
+    fromDate = date(int(fromDate[0]), int(fromDate[1]), int(fromDate[2]))
+
+    toDate = toDate.split("-")
+    toDate = date(int(toDate[0]), int(toDate[1]), int(toDate[2]))
+
+    query = ("""
+        SELECT Registro_temp.fecha, Registro_temp.hora, medida_temp, medida_humedad, medida_gas FROM Registro_temp
+            INNER JOIN Registro_humedad on Registro_humedad.fecha = Registro_temp.fecha AND Registro_humedad.hora = Registro_temp.hora
+            INNER JOIN Registro_gas on Registro_temp.fecha = Registro_gas.fecha AND Registro_temp.hora = Registro_gas.hora
+            WHERE Registro_temp.fecha BETWEEN %s AND %s
+            ORDER BY Registro_temp.fecha DESC, Registro_temp.hora DESC;
+    """)
+    try:
+        cursor.execute(query, (fromDate, toDate))
+    except:
+        print(f"ERROR en el rango de fechas!")
+
+    rows = cursor.fetchall()
+    conn.close()
+
+    data = DataFrame(rows)
+
+    return data
+
+
+
+data = get_measured_data("2025-11-26","2025-11-26")
+print(data)
